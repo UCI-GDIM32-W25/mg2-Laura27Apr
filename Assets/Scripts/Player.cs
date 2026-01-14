bb39d581ae3a1b8ba0ba2cdd9a2830e6149fb74d
Laura27Apr <leyangl4@uci.edu>
2026-01-13 19:23:17 -0800

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,27 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 5.0f;
     [SerializeField] private float _jumpForce = 8.0f;
+    [SerializeField] private float _jump;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private GameObject _coinPrefab;
+    [SerializeField] private Collider2D _collider;
+    [SerializeField] private Rigidbody2D _rigidbody;
 
-    private void Start()
-    {
-        _playerTransform = transform;
+    private bool _isGrounded = true;
 
-    }
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _isGrounded = false;
 
+            _rigidbody.velocity = new Vector2(
+                _rigidbody.velocity.x,
+                _jump
+            );
+        }
     }
+
+    
 }
