d7c9bcbbec059435d72009b7242100f08f6e20a8
Laura27Apr <leyangl4@uci.edu>
2026-01-13 18:46:07 -0800

    1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1.0f;
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private GameObject _coinPrefab;
+
+    private void Start()
+    {
+        _playerTransform = transform;
+
+        
+    }
+
+}
